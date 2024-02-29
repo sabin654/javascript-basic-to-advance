37cf7293e1652acfb09cc14f4d1b5d2d391e004b
sabin bhandari <sabinbhandari86@gmail.com>
2024-02-29 10:58:14 +0545

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict "; //treat all js as a new version or new js
+
+// alert("what is ur name "); // we are using nodejs not a browser , thats why it will not work in here
+// code readailiilty is important in code
+
+let name = " sabin";
+let age = 23;
+let isHelthy = true;
+
+// Primitive datatypes ,
+
+// number=
+//String =""
+//boolean="true" or "fase"
+//null = standalone value
+//undefined =>
+//symbol=> unique
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+//if u see the typeof null it will output => object so, null is object in js
+//typeof help to knw which datatype are present
