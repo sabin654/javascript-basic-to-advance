96b69fe750c6667c050ad618af3ce9e05791b52b
sabin bhandari <sabinbhandari86@gmail.com>
2024-03-02 16:42:07 +0545

    datatype summary

---
+++ 01_basic/05_datatypesSummery.js
@@ -0,0 +1,33 @@
+// Primitive datatypes ,
+
+// number=
+//String =""
+//boolean="true" or "fase"
+//null = standalone value (empty but not zer0)
+//undefined =>
+//symbol=> unique
+//bigInt
+
+const id = Symbol("1");
+const aid = Symbol("1");
+// console.log(id === aid);
+
+const bigNumber = 10220033030300300003n; //its big int
+
+// reference types or nonprimitive datatypes
+//array ,Objects, functions
+
+//all the non primitive datatypes are object  in js
+const hero = ["sabin", "shakti", "rahul"];
+
+const obj = {
+  name: "hey",
+  aage: 10,
+};
+
+function a() {
+  console.log("hello");
+}
+console.log(typeof hero); //object datatype
+console.log(typeof obj); //object
+console.log(typeof a); //function object
