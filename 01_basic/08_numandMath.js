d86d1b960d69148ac5cf80198ab6204517c35e80
sabin bhandari <sabinbhandari86@gmail.com>
2024-03-04 19:34:46 +0545

    Number and Math

---
+++ 01_basic/08_numandMath.js
@@ -0,0 +1,38 @@
+// const num = 5000;
+// const balance = new Number(100.02932323);
+// // console.log(typeof balance);
+
+// // console.log(balance.toString());
+// // console.log(balance.toFixed(2)); //widely used
+
+// const anotherNumber = 1123.0932;
+// console.log(anotherNumber.toPrecision(4)); // output will be 1123
+// console.log(anotherNumber.toPrecision(2)); // output will be 1.1e+3
+// console.log(anotherNumber.toPrecision(7)); // output will be 1123.093
+
+// const hundreads = 1000000;
+// console.log(hundreads.toLocaleString("en-NP")); //output willl be 1,000,000
+
+// +++++++++++++++++++++++++MAth+++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-200)); // abs= negative value to postive
+// console.log(Math.round(2.9)); // output will be 9  because it rounds up if .5 or more
+// console.log(Math.round(2.3)); //  //0utput will be 2  because it round down if .5 or less
+// console.log(Math.sqrt(25)); // square root of number
+// console.log(Math.max(1, 9, 2, 3)); // returns the highest number from given parameters
+// console.log(Math.min(1, 2, 3, 8)); //returns the lowest number from given parameter
+// console.log(Math.floor(2.9)); // return largest integer not greater than a given number //output will be 2
+
+// console.log(Math.random()); // generates random decimal between 0 and 1
+// console.log(Math.floor(Math.random() * 5 + 1));
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min)); // generate random numbers within a range (imprtant formula)
+
+// const mins = 5;
+// const maxs = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
