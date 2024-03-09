bd44a28be423a3548b4cb162f7dde6acf268c9d6
sabin bhandari <sabinbhandari86@gmail.com>
2024-03-09 21:06:16 +0545

    Object1

---
+++ 02_basic/03_object.js
@@ -0,0 +1,28 @@
+//singleton
+//==============================
+//object literal
+
+const sym = Symbol("key1");
+
+const user = {
+  name: "sabin bhandari",
+  "full name": "sabin bhandari",
+  [sym]: "I am private",
+  age: 25,
+  city: "ktm",
+  country: "Nepal",
+  lastlogin: ["sunday", "monday"],
+  gretting: function () {
+    console.log(`hello ${this.name} ! Your Age  is ${this.age}`);
+  },
+};
+// console.log(user.age); // not a good way to access object
+// console.log(user["name"]); //good  way of accessing the property in an object
+// console.log(user["full name"]);
+// console.log(user["lastlogin"][0]); //accessing array inside the object
+// console.log(user[sym]); // best way to acess Symbol
+
+// user.name = "sabin Bhandari";
+// Object.freeze(user); // this will freeze it means name sabin bhandari will never be chnage
+// console.log(user);
+user.gretting(); // call the greeting function
