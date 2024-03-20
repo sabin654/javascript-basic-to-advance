18919079a4d7b5fc699aace4c82fc427aecf3cf6
sabin bhandari <sabinbhandari86@gmail.com>
2024-03-20 12:58:51 +0545

    control flow

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const week = 2;
+switch (week) {
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+    console.log("monday");
+    break;
+  case 3:
+    console.log("thurday");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
+const weeks = "monday";
+switch (weeks) {
+  case "sunday":
+    console.log("sunday");
+    break;
+  case "monday":
+    console.log("monday");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
