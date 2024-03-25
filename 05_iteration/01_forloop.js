b017cae6f82b66040ca858fab2cc4581bd8aa614
sabin bhandari <sabinbhandari86@gmail.com>
2024-03-25 10:22:28 +0545

    forloop

---
+++ 05_iteration/01_forloop.js
@@ -0,0 +1,14 @@
+// for (let index = 0; index < 10; index++) {
+//   const element = index;
+//   if (element == 5) {
+//     console.log("wait");
+//   }
+//   console.log(element);
+// }
+
+// for (i = 1; i <= 10; i++) {
+//   console.log("the i is working ", i);
+//   for (j = 1; j <= 10; j++) {
+//     console.log(i + "*" + j + "=", i * j);
+//   }
+// }
